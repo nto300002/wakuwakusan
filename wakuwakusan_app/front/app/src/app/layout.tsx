132964d6cff1d0e67cb8187965def501a53b16d5
nto300002 <samonkntd@gmail.com>
2023-09-06 22:21:27 +0900

    fix toppage conponent

--- wakuwakusan_app/front/app/src/app/layout.tsx
+++ wakuwakusan_app/front/app/src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          {children}
+        </main>
       </body>
     </html>
   );
