0346a1706bd3ac05e4643d99579bea336fb4548f
nto300002 <samonkntd@gmail.com>
2023-09-07 21:46:25 +0900

    add maincontent css

---
+++ wakuwakusan_app/front/app/src/components/layouts/MainContent.tsx
@@ -0,0 +1,9 @@
+const MainContent = ({children}:any) => {
+  return (
+    <>
+      <div className="w-10/12 h-full text-gray-800 bg-white p-32 rounded-2xl">{children}</div>
+    </>
+        );
+}
+export default MainContent;
+
